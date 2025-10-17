9b7fdfc4588e9891f6a13e5d8d30579393c34cf4
Simon Håkansson <simon@delusional.enterprises>
2025-10-17 08:47:40 +0200

    feat(loops): implement  function to list negative cubes

--- loops.py
+++ loops.py
@@ -4,7 +4,7 @@
 numbers = [15, -5, -12, 7, 10, -7, 3, -10, 4] 
 
 
-
+# ----------[FUNCTIONS]----------
 def absoluteSum(numbers:list[int]) -> int:
     sum = 0
     for number in numbers:
@@ -12,4 +12,19 @@ def absoluteSum(numbers:list[int]) -> int:
             sum += number
     return sum
 
+def negativeCubes(numbers:list[int]) -> list[int]:
+    cubeList = []
+    for number in numbers:
+        if number < 0:
+            cubeList.append(number)
+    return cubeList
+
+
+
+
+
+# ----------[MAIN]----------
+
 print(f"the sum of numbers with an absolute value over 10 is {absoluteSum(numbers)}")
+print(negativeCubes(numbers))
+
